adfc2aa0722e0357e3b2ceed2c6bdd243c8dfd8f
moathssnck <myneww00@gmail.com>
2025-08-31 09:45:56 +0300

    dsa

--- lib/firestore.ts
+++ lib/firestore.ts
@@ -4,14 +4,14 @@ import { getFirestore } from "firebase/firestore";
 import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBalb45hYmw3rGK3kn5Skp2Wb4Ci3yeKHc",
-  authDomain: "adsapp-for.firebaseapp.com",
-  databaseURL: "https://adsapp-for-default-rtdb.firebaseio.com",
-  projectId: "adsapp-for",
-  storageBucket: "adsapp-for.firebasestorage.app",
-  messagingSenderId: "282166621415",
-  appId: "1:282166621415:web:2be67338bc64233153de42",
-  measurementId: "G-PG5NPD4FES"
+  apiKey: "AIzaSyC6YYfClSDS8JIDDLhRYQsUQb7DTrCR7_4",
+  authDomain: "new-swidah.firebaseapp.com",
+  databaseURL: "https://new-swidah-default-rtdb.firebaseio.com",
+  projectId: "new-swidah",
+  storageBucket: "new-swidah.firebasestorage.app",
+  messagingSenderId: "139797141578",
+  appId: "1:139797141578:web:85f9352e248cf25eab2eb3",
+  measurementId: "G-ZC14VFDDJ1"
 };
 
 const app = getApps().length > 0 ? getApp() : initializeApp(firebaseConfig);
