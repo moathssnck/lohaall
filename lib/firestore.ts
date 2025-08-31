956d775295a5a57ccb17c2c2e368e1ee73ba4a90
moathssnck <myneww00@gmail.com>
2025-08-31 07:23:22 +0300

    sss

--- lib/firestore.ts
+++ lib/firestore.ts
@@ -4,12 +4,14 @@ import { getFirestore } from "firebase/firestore";
 import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBG9kn0oW9gTrwf2ic22q4u8rSPE-2fcxI",
-  authDomain: "ofofoof-55d1c.firebaseapp.com",
-  projectId: "ofofoof-55d1c",
-  storageBucket: "ofofoof-55d1c.firebasestorage.app",
-  messagingSenderId: "388295345422",
-  appId: "1:388295345422:web:017cbb2ae4761bdbc8435f",
+  apiKey: "AIzaSyBalb45hYmw3rGK3kn5Skp2Wb4Ci3yeKHc",
+  authDomain: "adsapp-for.firebaseapp.com",
+  databaseURL: "https://adsapp-for-default-rtdb.firebaseio.com",
+  projectId: "adsapp-for",
+  storageBucket: "adsapp-for.firebasestorage.app",
+  messagingSenderId: "282166621415",
+  appId: "1:282166621415:web:2be67338bc64233153de42",
+  measurementId: "G-PG5NPD4FES"
 };
 
 const app = getApps().length > 0 ? getApp() : initializeApp(firebaseConfig);
